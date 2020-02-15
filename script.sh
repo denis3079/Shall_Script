1ad08cfd23c56c9ae2b7f7d25a91d0edfe97fd61
Denis Arbatov <denis3079@yandex.ru>
2020-02-16 01:04:09 +0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+then
+   echo "Используйте: $0 два параметра командной строки 1.txt 2.txt"
+   exit
+else
+   f1="$1"
+   f2="$2"
+fi
+#f1="1.txt"
+#f2="2.txt"
+echo "======"
+n1=""
+n2=""
+LN=1
+paste $1 $2 | while read n1 n2 ; do
+    expr $n1 + 0 >/dev/null 2>&1 
+    [ $? -gt 1 ] && echo Ошибка в $1 строка $LN: $n1 && exit 1
+    expr $n1 + $n2
+    [ $? -gt 1 ] && echo Ошибка в $2 строка $LN: $n2 && exit 1   
+done
+echo "======"
+    awk '{total+=$1} END{print total}' 1.txt 2.txt  
+
